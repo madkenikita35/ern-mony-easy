2918fea91c0521ffad8ad327a227d052f9e53022
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-30 23:25:56 +0530

    Add Changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -260,9 +260,9 @@ const Login = () => {
       <div className="absolute inset-0 bg-black/30"></div>
 
       {/* Main Card Container */}
-      <div className="relative z-10 flex flex-col md:flex-row w-full max-w-4xl bg-white/20 backdrop-blur-md rounded-3xl shadow-2xl overflow-hidden min-h-[550px]">
+      <div className=" relative z-10 flex flex-col md:flex-row w-full max-w-4xl bg-white/20 backdrop-blur-md rounded-3xl shadow-2xl overflow-hidden min-h-[550px]">
         {/* LEFT SIDE: Image Section */}
-        <div className="hidden md:flex md:w-1/2 relative overflow-hidden">
+        <div className=" hidden md:flex md:w-1/2 relative overflow-hidden">
           <img
             src="https://images.unsplash.com/photo-1556761175-b413da4baf72?q=80&w=1974&auto=format&fit=crop"
             alt="Office"
