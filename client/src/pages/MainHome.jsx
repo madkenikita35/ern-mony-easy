a2af4c80f8e8ce5d89bcc048a7fb6565adae0907
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-17 22:36:54 +0530

    Add

--- client/src/pages/MainHome.jsx
+++ client/src/pages/MainHome.jsx
@@ -18,7 +18,7 @@ const MainHome = () => {
 
       <div className=" relative z-10 w-full max-w-5xl">
         {/* Top Header Section */}
-        <div className=" text-center mb-10">
+        <div className=" text-center mb-10 ">
           <h1 className=" text-5xl font-extrabold text-white mb-4 tracking-tight ">
             Welcome, <span className=" text-blue-400">{user?.name}</span>
           </h1>
