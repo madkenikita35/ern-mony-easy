e70bf907fe41462b84b2772a0559b164515ccd2e
Nikita Satish Madke <madkenikita35@gmail.com>
2026-06-19 23:36:32 +0530

    Add

--- client/src/pages/AdminDash.jsx
+++ client/src/pages/AdminDash.jsx
@@ -75,7 +75,7 @@ const AdminDash = () => {
                 <th className=" p-4 ">Role</th>
                 <th className=" p-4 ">Trial Start</th>
                 <th className=" p-4 ">Status</th>
-                <th className=" p-4">Action</th>
+                <th className=" p-4 ">Action</th>
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-700 ">
